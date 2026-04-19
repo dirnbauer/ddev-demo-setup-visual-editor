7e4d6848e6c0730235ab18cfb8846def6223ba55
Kurt Dirnbauer <dirnbauer@webconsulting.at>
2026-04-19 13:40:55 +0200

    Add shared admin panel defaults

---
+++ packages/adminpanel_defaults/ext_emconf.php
@@ -0,0 +1,19 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Admin Panel Defaults',
+    'description' => 'Shared TYPO3 admin panel defaults for this demo project.',
+    'category' => 'misc',
+    'author' => 'Codex',
+    'author_email' => '',
+    'state' => 'beta',
+    'version' => '0.1.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '14.0.0-14.99.99',
+            'adminpanel' => '14.0.0-14.99.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
